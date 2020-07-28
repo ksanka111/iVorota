1631f845925fe73f6e56cd7e03962ca1da649a1b
aksana1984 <zdarskay@gmail.com>
2020-07-28 18:23:06 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,30 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    pug  = require('gulp-pug'),
-    browserSync = require('browser-sync').create()
+  sass = require('gulp-sass'),
+  pug = require('gulp-pug'),
+  browserSync = require('browser-sync').create()
 
-gulp.task('pug', function() {
+gulp.task('pug', function () {
   return gulp.src('src/pug/**/*.pug')
-  .pipe(pug({
-    pretty: true
-  }))
-  .pipe(gulp.dest('build'))
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('build'))
 });
 
-gulp.task('sass', function() {
-  return gulp.src('src/sass/**/*.scss')
-  .pipe(sass({
-    // outputStyle: 'compressed'
-  }))
-  .pipe(gulp.dest('build/css'))
+gulp.task('sass', function () {
+  return gulp.src('src/sass/**/*.sass')
+    .pipe(sass({
+      // outputStyle: 'compressed'
+    }))
+    .pipe(gulp.dest('build/css'))
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch('src/pug/**/*.pug', gulp.series('pug'));
-  gulp.watch('src/sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('src/sass/**/*.sass', gulp.series('sass'));
 });
 
-gulp.task('serve', function() {
+gulp.task('serve', function () {
   browserSync.init({
     server: {
       baseDir: "./build"
